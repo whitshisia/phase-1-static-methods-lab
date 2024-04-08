78a230eea77c0c55e89ece2b79c5610ce04c467d
whitney shisia <shisiawhitney215@gmail.com>
2024-04-08 19:25:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
-}
+  
+    static capitalize(str) {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+  
+    // Static method to sanitize a string
+    static sanitize(str) {
+      return str.replace(/[^a-zA-Z0-9-' ]/g, '');
+    }
+  
+    // Static method to titleize a string
+    static titleize(str) {
+      const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+      const words = str.split(' ');
+      const titleizedWords = words.map((word, index) => {
+        if (index === 0 || !exceptions.includes(word.toLowerCase())) {
+          return this.capitalize(word);
+        } else {
+          return word;
+        }
+      });
+      return titleizedWords.join(' ');
+    }
+  }
+  
